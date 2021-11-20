a745fd1de675029a120bc1809675c4de287a7add
Ana <anacbarboto@gmail.com>
2021-11-19 19:26:30 -0500

    igual que el primer commit

--- selenium_prueba.py
+++ selenium_prueba.py
@@ -19,14 +19,23 @@ class PythonOrgSearch(unittest.TestCase):
         driver.get("https://www.espol.edu.ec/es/educacion")
         facultades = WebDriverWait(driver, 20).until(EC.visibility_of_all_elements_located((By.XPATH, '//div[@class="panel-heading"]//h4[@class="panel-title"]//a')))
         
+        i = 0
         for facultad in facultades:
+            i = i + 1
             print(facultad.get_attribute("href").split("#")[-1])
             print(facultad.text)
 
-            todas_las_materias = driver.find_elements(By.XPATH, '//div[@id="'+facultad.get_attribute("href").split("#")[-1]+'"]//div[@class="field-content"]//ul//li//a')
-            for materia in todas_las_materias:
-                print(materia.get_attribute("href"))
-                print(materia.text)
+            if (i > 2):
+                driver.execute_script("arguments[0].scrollIntoView();", facultad)
+
+            driver.execute_script("arguments[0].click();", facultad)
+            ActionChains(driver).click_and_hold(facultad).perform()
+
+            if (facultad.get_attribute("aria-expanded")):
+                todas_las_materias = driver.find_elements(By.XPATH, '//div[@id="'+facultad.get_attribute("href").split("#")[-1]+'"]//div[@class="field-content"]//ul//li//a')
+                for materia in todas_las_materias:
+                    print(materia.get_attribute("href"))
+                    print(materia.text)
 
     def tearDown(self):
         self.driver.close()
