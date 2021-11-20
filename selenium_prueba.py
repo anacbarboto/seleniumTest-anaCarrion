adb999f88df00e3a094c76271c1413912db9ed1f
Ana <anacbarboto@gmail.com>
2021-11-19 19:24:05 -0500

    tengo todas las facultades y el link de las materias, no tengo los nombres porque falta click

--- selenium_prueba.py
+++ selenium_prueba.py
@@ -17,46 +17,16 @@ class PythonOrgSearch(unittest.TestCase):
         driver = self.driver
         driver.maximize_window()
         driver.get("https://www.espol.edu.ec/es/educacion")
-        facultades_izquierda = WebDriverWait(driver, 20).until(EC.visibility_of_all_elements_located((By.XPATH, '//section[@id="block-views-facultades-block"]//h4[@class="panel-title"]//a')))
+        facultades = WebDriverWait(driver, 20).until(EC.visibility_of_all_elements_located((By.XPATH, '//div[@class="panel-heading"]//h4[@class="panel-title"]//a')))
         
-        i = 0
-        for facultad in facultades_izquierda:
-            i = i + 1
+        for facultad in facultades:
             print(facultad.get_attribute("href").split("#")[-1])
             print(facultad.text)
-            
-            #facultad.location_once_scrolled_into_view
-            if (i > 2):
-                driver.execute_script("arguments[0].scrollIntoView();", facultad)
-            
-            driver.execute_script("arguments[0].click();", facultad)
-            ActionChains(driver).click_and_hold(facultad).perform()
-            #ActionChains(driver).release(facultad).perform()
-            
-            #driver.execute_script("arguments[0].setAttribute('aria-expanded','true')", facultad)
-            if (facultad.get_attribute("aria-expanded")):
-                todas_las_materias = driver.find_elements(By.XPATH, '//div[@id="'+facultad.get_attribute("href").split("#")[-1]+'"]//div[@class="field-content"]//ul//li//a')
-                for materia in todas_las_materias:
-                    print(materia.get_attribute("href"))
-                    print(materia.text)
-            print("-------------------------------------------------")
-        
-        facultades_derecha = driver.find_elements(By.XPATH, '//section[@id="block-views-facultades-block-1"]//h4[@class="panel-title"]//a')
-        j = 0
-        for f in facultades_derecha:
-            j = j + 1
-            print(f.get_attribute("href").split("#")[-1])
-            print(f.text)
 
-            ActionChains(driver).move_to_element(f).click().perform()
-            
-            #driver.execute_script("arguments[0].setAttribute('aria-expanded','true')", facultad)
-            if (f.get_attribute("aria-expanded")):
-                todas_las_materias = driver.find_elements(By.XPATH, '//div[@id="'+f.get_attribute("href").split("#")[-1]+'"]//div[@class="field-content"]//ul//li//a')
-                for materia in todas_las_materias:
-                    print(materia.get_attribute("href"))
-                    print(materia.text)
-            print("-------------------------------------------------")
+            todas_las_materias = driver.find_elements(By.XPATH, '//div[@id="'+facultad.get_attribute("href").split("#")[-1]+'"]//div[@class="field-content"]//ul//li//a')
+            for materia in todas_las_materias:
+                print(materia.get_attribute("href"))
+                print(materia.text)
 
     def tearDown(self):
         self.driver.close()
